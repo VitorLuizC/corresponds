6dc10096477c2f1949262f75160940b5a936ecb7
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2018-07-05 23:04:29 -0300

    Create tests

---
+++ test/index.ts
@@ -0,0 +1,39 @@
+import test from 'ava';
+import { corresponds, correspondsToLeft, correspondsToRight } from '../';
+
+test('Module API', (context) => {
+  context.is(typeof corresponds, 'function');
+  context.is(typeof correspondsToLeft, 'function');
+  context.is(typeof correspondsToRight, 'function');
+});
+
+const schema = {
+  'name': 'user.name'
+};
+
+test('Corresponds returns .toLeft and .toRight functions', (context) => {
+  const correspondent = corresponds(schema);
+
+  context.true(typeof correspondent.toLeft, 'function');
+  context.true(typeof correspondent.toRight, 'function');
+});
+
+const valueA = {
+  name: 'Carlos'
+};
+
+const valueB = {
+  user: {
+    name: 'Carlos'
+  }
+};
+
+test('Maps object to schema\'s left-side', (context) => {
+  context.deepEqual(corresponds(schema).toLeft(valueA), valueB);
+  context.deepEqual(correspondsToLeft(schema, valueA), valueB);
+});
+
+test('Maps object to schema\'s right-side', (context) => {
+  context.deepEqual(corresponds(schema).toRight(valueB), valueA);
+  context.deepEqual(correspondsToRight(schema, valueB), valueA);
+});
